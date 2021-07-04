f0cd6efdcafc2483f2ac82d285065ba9dee39c48
David Biró <david.biro@stylersonline.com>
2021-07-04 20:32:38 +0200

    Add desktop setup script

---
+++ .config/bspwm/setup_desktops.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+MONITORS=($(xrandr | grep -Po "^[^ ]+ (connected)" | cut -d' ' -f1 | sort -r))
+
+# For one monitor I create 5 desktops
+[[ "${#MONITORS[@]}" == 1 ]] && {
+	FIRST_MONITOR=${MONITORS[0]}
+	bspc monitor "${FIRST_MONITOR}" -d \
+		     "${FIRST_MONITOR}1" \
+		     "${FIRST_MONITOR}2" \
+		     "${FIRST_MONITOR}3" \
+		     "${FIRST_MONITOR}4" \
+		     "${FIRST_MONITOR}5"
+}
+
+# For 2 or more monitors, create one for first and one for second
+# TODO: This should check the real size of the monitor, 
+#		setup 5 desktops for the biggest 3 for each other.
+[[ "${#MONITORS[@]}" != 1 ]] && {
+	SECOND_MONITOR=${MONITORS[1]}
+	REST_OF_MONITOR=${MONITORS[@]:2}
+
+	bspc monitor "${FIRST_MONITOR}" -d \
+		"${FIRST_MONITOR}1"
+	bspc monitor "${SECOND_MONITOR}" -d \
+		"${SECOND_MONITOR}1" \
+		"${SECOND_MONITOR}2" \
+		"${SECOND_MONITOR}3" \
+		"${SECOND_MONITOR}4" \
+		"${SECOND_MONITOR}5"
+	for MONITOR in $REST_OF_MONITOR; do
+		bspc monitor $MONITOR -d \
+			"${MONITOR}1" \
+			"${MONITOR}2" \
+			"${MONITOR}3"
+	done
+}
